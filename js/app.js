38b41138b385862bde2721ba51737a01c5ed8626
Guest1 <my@email.comgit config --globak username my>
2020-05-04 12:12:04 -0400

    fixed issue with button and loading

--- js/app.js
+++ js/app.js
@@ -1,19 +1,5 @@
 window.onload = function() {
+  alert("You are about to take a quiz.")
+  alert("Please press classic quiz to begin.")
   document.getElementById("quiz1").onclick = quiz;
-  let response;
 }
-const quiz = function() {
-  alert("You are about to take a quiz.");
-  response = prompt("Would you like to take the quiz?")
-  response = response.toLowerCase;
-  if (response == "yes") {
-    // trying to link to quiz.html page where we will start to pose q's
-    button.innerhtml = "href = quiz.html"
-    }
-  else if (response == "no") {
-    window.onsubmit("index.html")
-  }
-  else if (response == null) {
-    // on purpose
-  }
-  }
